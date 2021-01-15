60d0efa9a61471bd94763618e2b0b143ac0fd2c4
hachi-eiji <hachiyae@gmail.com>
2021-01-15 21:52:52 +0900

    3 files modified

--- app.js
+++ app.js
@@ -1,22 +1,16 @@
-import angular from 'angular';
-import '@uirouter/angularjs';
-import 'ng-custom-element';
+import angular from "angular";
+import "@uirouter/angularjs";
 
 // Import your app stylesheets
-import './style.css';
+import "./style.css";
 
 // Import your app functionality
-import './home'
-import './sandbox-elements.main.js'
+import "./home";
+import "./sandbox-elements.main.js";
 
 // Create and bootstrap application
-const requires = [
-  'ngCustomElement',
-  'ui.router',
-  'home',
-];
-window.initializeAngularElements().then(function(){
-  window.app = angular.module('app', requires);
-  angular.bootstrap(document.getElementById('app'), ['app']);
-})
-
+const requires = ["ui.router", "home"];
+window.initializeAngularElements().then(function() {
+  window.app = angular.module("app", requires);
+  angular.bootstrap(document.getElementById("app"), ["app"]);
+});
